e2a6b45cba5388b6db73e5b055c0a21ba05fa492
Asliddin Abdivasiyev <asliddin.abdivasiyev@gmail.com>
2023-01-09 18:47:06 +0500

    fix: remove folding

--- lua/options.lua
+++ lua/options.lua
@@ -11,21 +11,17 @@ vim.wo.number            = true
 vim.wo.signcolumn        = 'yes'
 vim.wo.cursorline        = true
 
--- Fold
-vim.o.foldmethod         = 'syntax'
-vim.o.foldlevel          = 1
-vim.o.foldclose          = 'all'
-
 -- Behaviour
 vim.o.hlsearch           = false
+vim.o.incsearch          = true
 vim.o.ignorecase         = true                         -- Ignore case when using lowercase in search
 vim.o.smartcase          = true                         -- But don't ignore it when using upper case
 vim.o.smarttab           = true
 vim.o.smartindent        = true
 vim.o.expandtab          = true                         -- Convert tabs to spaces.
-vim.o.tabstop            = 2
-vim.o.softtabstop        = 2
-vim.o.shiftwidth         = 2
+vim.o.tabstop            = 4
+vim.o.softtabstop        = 4
+vim.o.shiftwidth         = 4
 vim.o.splitbelow         = true
 vim.o.splitright         = true
 vim.o.scrolloff          = 12                           -- Minimum offset in lines to screen borders
